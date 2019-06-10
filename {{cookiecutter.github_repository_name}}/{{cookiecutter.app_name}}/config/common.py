78969e5c4f580d06404e046722cb6fa7d98c1e6f
Pamfilos Fokianos <pamfilosf@gmail.com>
2019-06-11 00:25:44 +0200

    setup: adds Pipfile + updates for translation
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/common.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/common.py
@@ -64,7 +64,7 @@ class Common(Configuration):
     LANGUAGE_CODE = 'en-us'
     # If you set this to False, Django will make some optimizations so as not
     # to load the internationalization machinery.
-    USE_I18N = False
+    USE_I18N = True
     USE_L10N = True
     USE_TZ = True
     LOGIN_REDIRECT_URL = '/'
